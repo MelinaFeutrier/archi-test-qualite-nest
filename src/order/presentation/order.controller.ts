42c3f524a883a6635d5b8092979b10c936ca3d0a
MelinaFeutrier <melina.feutrier@gmail.com>
2024-10-02 15:29:11 +0200

    use of repository interface

--- src/order/presentation/order.controller.ts
+++ src/order/presentation/order.controller.ts
@@ -5,14 +5,12 @@ import {
 } from 'src/order/domain/entity/order.entity';
 import { CreateOrderService } from 'src/order/domain/use-case/create-order.service';
 import { PayOrderService } from '../domain/use-case/pay-order.service';
-import { OrderManagerService } from '../crado/order-manager.service';
 
 @Controller('/orders')
 export default class OrderController {
   constructor(
     private readonly createOrderService: CreateOrderService,
     private readonly payOrderService: PayOrderService,
-    private readonly orderManagerService: OrderManagerService,
   ) {}
 
   @Post()
@@ -26,8 +24,5 @@ export default class OrderController {
   async payOrder(@Param('id') id: string): Promise<Order> {
     return await this.payOrderService.execute(id);
   }
-  @Post(':id/process')
-  async processOrder(@Param('id') orderId: string): Promise<void> {
-    await this.orderManagerService.processOrder(orderId);
-  }
+
 }
